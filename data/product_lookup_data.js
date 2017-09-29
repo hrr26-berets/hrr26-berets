c6053eb772efbb970afadbb892ac84579c8c359b
Jacob Yoo <jacobyoo@gmail.com>
2017-09-29 11:09:51 -0700

    added mock data

---
+++ data/product_lookup_data.js
@@ -0,0 +1,40 @@
+// Example of product look up (by itemId) response
+// documentation: https://developer.walmartlabs.com/docs
+
+
+{
+    "itemId": 12417832,
+    "parentItemId": 12417832,
+    "name": "MLB &#045; Clear Acrylic Baseball Case",
+    "upc": "10001137891",
+    "categoryPath": "Sports & Outdoors/Sports Fan Shop/For the Home Fan Shop/Decor Fan Shop/Decorative Accessories Fan Shop",
+    "longDescription": "The deluxe ball cube is perfect for a collectible baseball. It is made of 1/8th inch thick acrylic and has a black acrylic base with a small clear acrylic removable lid. Measures: 5&quot; x 4 1/4 &quot; x 4.&quot; Memorabilia sold separately. &lt;br&gt;&lt;b&gt;Please Note:&lt;/b&gt;&lt;br&gt;This item has handling time. If an upgraded shipping method is chosen, the handling time shown will still apply.",
+    "brandName": "Mounted Memories",
+    "thumbnailImage": "https://i5.walmartimages.com/asr/a95cdcd3-a239-4a69-a6c0-696a9858ac8e_1.749db39cdd0357817252ae1060e0ebac.jpeg?odnHeight=100&odnWidth=100&odnBg=FFFFFF",
+    "mediumImage": "https://i5.walmartimages.com/asr/a95cdcd3-a239-4a69-a6c0-696a9858ac8e_1.749db39cdd0357817252ae1060e0ebac.jpeg?odnHeight=180&odnWidth=180&odnBg=FFFFFF",
+    "largeImage": "https://i5.walmartimages.com/asr/a95cdcd3-a239-4a69-a6c0-696a9858ac8e_1.749db39cdd0357817252ae1060e0ebac.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF",
+    "productTrackingUrl": "http://linksynergy.walmart.com/fs-bin/click?id=jacobyoo&offerid=223073.7200&type=14&catid=8&subid=0&hid=7200&tmpid=1082&RD_PARM1=http%253A%252F%252Fwww.walmart.com%252Fip%252FClear-Acrylic-Baseball-Case%252F12417832%253Faffp1%253DV0OkussvRn6ZTCyr4Q2wWe0PvIK_IpXRhtB1gyO6U00%2526affilsrc%253Dapi",
+    "standardShipRate": 5,
+    "marketplace": true,
+    "sellerInfo": "Pro Team",
+    "productUrl": "http://c.affil.walmart.com/t/api01?l=http%3A%2F%2Fwww.walmart.com%2Fip%2FClear-Acrylic-Baseball-Case%2F12417832%3Faffp1%3DV0OkussvRn6ZTCyr4Q2wWe0PvIK_IpXRhtB1gyO6U00%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi",
+    "customerRating": "4.5",
+    "customerRatingImage": "http://i2.walmartimages.com/i/CustRating/4_5.gif",
+    "categoryNode": "4125_1063984_1063994",
+    "bundle": false,
+    "clearance": false,
+    "preOrder": false,
+    "stock": "NOT_AVAILABLE",
+    "addToCartUrl": "http://c.affil.walmart.com/t/api01?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D12417832%7C1%26affp1%3DV0OkussvRn6ZTCyr4Q2wWe0PvIK_IpXRhtB1gyO6U00%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi",
+    "affiliateAddToCartUrl": "http://linksynergy.walmart.com/fs-bin/click?id=jacobyoo&offerid=223073.7200&type=14&catid=8&subid=0&hid=7200&tmpid=1082&RD_PARM1=http%253A%252F%252Faffil.walmart.com%252Fcart%252FaddToCart%253Fitems%253D12417832%257C1%2526affp1%253DV0OkussvRn6ZTCyr4Q2wWe0PvIK_IpXRhtB1gyO6U00%2526affilsrc%253Dapi",
+    "freeShippingOver50Dollars": false,
+    "imageEntities": [
+        {
+            "thumbnailImage": "https://i5.walmartimages.com/asr/a95cdcd3-a239-4a69-a6c0-696a9858ac8e_1.749db39cdd0357817252ae1060e0ebac.jpeg?odnHeight=100&odnWidth=100&odnBg=FFFFFF",
+            "mediumImage": "https://i5.walmartimages.com/asr/a95cdcd3-a239-4a69-a6c0-696a9858ac8e_1.749db39cdd0357817252ae1060e0ebac.jpeg?odnHeight=180&odnWidth=180&odnBg=FFFFFF",
+            "largeImage": "https://i5.walmartimages.com/asr/a95cdcd3-a239-4a69-a6c0-696a9858ac8e_1.749db39cdd0357817252ae1060e0ebac.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF",
+            "entityType": "PRIMARY"
+        }
+    ],
+    "availableOnline": false
+}
