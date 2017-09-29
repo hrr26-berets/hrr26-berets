9b11f84a5e1abd0ce6f9855ef1e3515cb2a773ab
Jacob Yoo <jacobyoo@gmail.com>
2017-09-29 16:55:51 -0700

    added thumbnail image to mock data, added bootstrap.min.css to index.html

--- src/client/app/Main.jsx
+++ src/client/app/Main.jsx
@@ -9,19 +9,19 @@ class Main extends Component {
     super(props);
     this.state = {
       popular: [
-      {_id: 1, name: 'pizza', salePrice: 3.99},
-      {_id: 2, name: 'pizza', salePrice: 3.99},
-      {_id: 3, name: 'pizza', salePrice: 3.99},
-      {_id: 4, name: 'pizza', salePrice: 3.99}
+      {_id: 1, name: 'pizza', salePrice: 3.99, thumbnailImage: 'https://i5.walmartimages.com/asr/2521fd6c-b947-4b2d-9a31-7887d1f1a0c0_1.659b2d798672c8f5e1338d929452b74e.jpeg?odnHeight=100&odnWidth=100&odnBg=FFFFFF'},
+      {_id: 2, name: 'pizza', salePrice: 3.99, thumbnailImage: 'https://i5.walmartimages.com/asr/2521fd6c-b947-4b2d-9a31-7887d1f1a0c0_1.659b2d798672c8f5e1338d929452b74e.jpeg?odnHeight=100&odnWidth=100&odnBg=FFFFFF'},
+      {_id: 3, name: 'pizza', salePrice: 3.99, thumbnailImage: 'https://i5.walmartimages.com/asr/2521fd6c-b947-4b2d-9a31-7887d1f1a0c0_1.659b2d798672c8f5e1338d929452b74e.jpeg?odnHeight=100&odnWidth=100&odnBg=FFFFFF'},
+      {_id: 4, name: 'pizza', salePrice: 3.99, thumbnailImage: 'https://i5.walmartimages.com/asr/2521fd6c-b947-4b2d-9a31-7887d1f1a0c0_1.659b2d798672c8f5e1338d929452b74e.jpeg?odnHeight=100&odnWidth=100&odnBg=FFFFFF'}
       ]
     };
   }
 
   render() {
     return (
-      <div>
+      <div className="container">
         <h1>wishList</h1>
-        <div><SearchBar /></div>
+        <div><SearchBar className="row"/></div>
         <PopularItems products={this.state.popular}/>
       </div>
       )
