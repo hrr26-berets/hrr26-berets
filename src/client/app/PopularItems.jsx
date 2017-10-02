a927b88f0b6966cb8a3b0cbb804394b123cc1897
Douglas Salazar <countertenor.salazar@gmail.com>
2017-10-01 20:59:14 -0400

    Fixed case on filenames.

---
+++ src/client/app/PopularItems.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Item from './Item.jsx';
+
+var PopularItems = (props) => (
+  <div className="popular-items">
+    {props.products.map(product =>
+      <Item onClick={props.onClick} product={product} key={product._id}/>
+      )}
+  </div>
+  )
+
+
+//render one box for each item
+//we want three now but could handle more or less
+//onClick passed from parent component allows user to follow url link to walmart product page
+//When styling make sure products are arranged horizontally.
+
+
+export default PopularItems;
