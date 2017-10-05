de3b2e63050ea0aea471950f4932ff5e44719a43
Jacob Yoo <jacobyoo@gmail.com>
2017-10-05 07:46:50 -0700

    couple of small fixes

--- src/client/app/FeaturedLists.jsx
+++ src/client/app/FeaturedLists.jsx
@@ -6,23 +6,23 @@ var FeaturedLists = (props) => {
     return (
       <div id="FL-Container">
         <div className="FL-col1">
-        <div className="aligning"> Electronics</div>
-        {props.list[3944].map(item =><FeaturedItem item={item} />) }
+          <div className="aligning"> Electronics</div>
+          {props.list[3944].map(item =><FeaturedItem item={item} key={item.itemId}/>) }
         </div>
 
-      <div className="FL-col1">
-        <div className="aligning"> Beauty</div>
-        {props.list[1085666].map(item =><FeaturedItem item={item} />) }
+        <div className="FL-col1">
+          <div className="aligning"> Beauty</div>
+          {props.list[1085666].map(item =><FeaturedItem item={item} key={item.itemId}/>) }
         </div>
 
-     <div className="FL-col1">
-        <div className="aligning"> Clothing</div>
-        {props.list[5438].map(item =><FeaturedItem item={item} />) }
-     </div>
+        <div className="FL-col1">
+          <div className="aligning"> Clothing</div>
+          {props.list[5438].map(item =><FeaturedItem item={item} key={item.itemId}/>) }
+        </div>
 
-     <div className ="FL-col1">
-        <div className="aligning"> Health </div>
-        {props.list[976760].map(item =><FeaturedItem item={item} />) }
+        <div className ="FL-col1">
+          <div className="aligning"> Health </div>
+          {props.list[976760].map(item =><FeaturedItem item={item} key={item.itemId}/>) }
      </div>
    </div>
   )
